24e0be9277556784c34761fd2f10695cd1a5e5b5
Antony Caron <antonycaron91@gmail.com>
2020-09-11 14:56:22 +0200

    7 - Les images et le inline CSS dans React.js

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
+import batman from '../../images/batman.png';
+
+const centerH2 = {
+    textAlign: 'center',
+    marginTop: '50px'
+}
+
+const centerImg = {
+    display: "block",
+    margin: "40px auto"
+}
 
 const ErrorPAge = () => {
     return (
         <div className="quiz-bg">
             <div className="container">
-                ErrorPage
+                <h2 style={centerH2}>Oups, cette page n'existe pas!</h2>
+                <img style={centerImg} src={batman} alt="error page"/>
             </div>
         </div>
     )
 }
 
-export default ErrorPAge
+export default ErrorPAge;
