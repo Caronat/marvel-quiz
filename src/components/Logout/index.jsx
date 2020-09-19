abe5bb22aadddaf791472fd085bbe917ed4d2f31
Antony Caron <antonycaron91@gmail.com>
2020-09-19 13:29:44 +0200

    Add tool-tip logout

--- src/components/Logout/index.jsx
+++ src/components/Logout/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import ReactTooltip from 'react-tooltip';
 import { FirebaseContext } from "../Firebase";
 
 const Logout = (props) => {
@@ -18,11 +19,11 @@ const Logout = (props) => {
 
   return (
     <div className="logoutContainer">
-      {/* <h2>Pseudo : {pseudo}</h2> */}
+      <ReactTooltip place="left" effect="solid"/>
 
       <label className="switch">
         <input type="checkbox" checked={checked} onChange={handleChange} />
-        <span className="slider round"></span>
+        <span className="slider round" data-tip="Déconnexion"></span>
       </label>
     </div>
   );
